752f196a12f341813db6ac0a42f12dbe0a3c1bb0
DunkaL <49835111+DunkaL@users.noreply.github.com>
2020-09-19 20:18:12 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var sliderVertical = $('#slider-vertical').lightSlider({
+        adaptiveHeight: true,
+        item: 1,
+        slideMargin: 0,
+        vertical: true,
+        speed: 700,
+        enableTouch: false,
+        enableDrag: false,
+        controls: false,
+        pager: false
+    });
+
+    $('#top-arrow').click(function(){
+        sliderVertical.goToPrevSlide(); 
+    });
+
+    $('#bottom-arrow').click(function(){
+        sliderVertical.goToNextSlide(); 
+    });
+
+    $('.slider-block').slick({
+        item: 1,
+        dots: true,
+        prevArrow:'<button type="button" class="btn btn-danger slider-block__btn" id="top-arrow"><i class="fas fa-angle-double-left"></i></button>',
+        nextArrow:'<button type="button" class="btn btn-danger slider-block__btn right-0" id="top-arrow"><i class="fas fa-angle-double-right"></i></button>'
+    })
+    
+});
