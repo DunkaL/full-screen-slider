d3fe7dd591a2744bda0891be13b57d71d7816310
DunkaL <49835111+DunkaL@users.noreply.github.com>
2020-09-20 13:35:59 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,43 +11,57 @@ document.addEventListener("DOMContentLoaded", function () {
         pager: false
     });
 
-    $('#top-arrow').click(function(){
-        sliderVertical.goToPrevSlide(); 
+    $('#top-arrow').click(function () {
+        sliderVertical.goToPrevSlide();
     });
 
-    $('#bottom-arrow').click(function(){
-        sliderVertical.goToNextSlide(); 
+    $('#bottom-arrow').click(function () {
+        sliderVertical.goToNextSlide();
     });
 
     $('.slider-block').slick({
         item: 1,
         dots: true,
-        prevArrow:'<button type="button" class="btn btn-danger slider-block__btn" id="top-arrow"><i class="fas fa-angle-double-left"></i></button>',
-        nextArrow:'<button type="button" class="btn btn-danger slider-block__btn right-0" id="top-arrow"><i class="fas fa-angle-double-right"></i></button>'
+        prevArrow: '<button type="button" class="btn btn-danger slider-block__btn" id="top-arrow"><i class="fas fa-angle-double-left"></i></button>',
+        nextArrow: '<button type="button" class="btn btn-danger slider-block__btn right-0" id="top-arrow"><i class="fas fa-angle-double-right"></i></button>'
     });
 
-    
-    function Height(){
+
+    function Height(block, takeAway) {
         let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
+        let height = vh * 100 - takeAway;
+        $(block).css("height",height+'px');
+    }
+    if (window.innerWidth > 500) {
+        Height('.content', 61);
+        Height('.slider-vertical__slide', 146);
+        Height('.slider-block__slide', 146);
+        window.addEventListener('resize', () => {
+            Height('.content', 61);
+            Height('.slider-vertical__slide', 146);
+            Height('.slider-block__slide', 146);
+        });   
+    }else{
+        Height('.content', 98);
+        Height('.slider-vertical__slide', 183);
+        Height('.slider-block__slide', 183);
+        window.addEventListener('resize', () => {
+            Height('.content', 98);
+            Height('.slider-vertical__slide', 183);
+            Height('.slider-block__slide', 183);
+        });   
     }
 
-    Height();
-    
-    window.addEventListener('resize', () => {
-        Height();
-    });
-
-    $('#button-sidebar').click(function(){
+    $('#button-sidebar').click(function () {
         $('#sidebar').toggleClass('bottom-0');
     });
 
-    $('#button-header-sidebar').click(function(){
+    $('#button-header-sidebar').click(function () {
         $('#sidebar').addClass('left-0');
     });
 
-    $('#button-sidebar-close').click(function(){
+    $('#button-sidebar-close').click(function () {
         $('#sidebar').removeClass('left-0');
     });
-    
+
 });
